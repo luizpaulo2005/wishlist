dd90d2e639a4bd073e3264e527de45c40aa53a02
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2024-02-16 20:27:40 -0300

    fix: remove formatted on desktop

--- src/lib/text-formatted.tsx
+++ src/lib/text-formatted.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useMediaQuery } from "@/lib/use-media-query";
+
 interface TextFormattedProps {
   text: string;
   length?: number;
@@ -5,10 +9,13 @@ interface TextFormattedProps {
 
 const TextFormatted = (props: TextFormattedProps) => {
   const { text, length } = props;
+  const isDesktop = useMediaQuery("(min-width: 768px)");
 
   return (
     <span className="text-nowrap">
-      {text.length > (length ? length : 15)
+      {isDesktop
+        ? text
+        : text.length > (length ? length : 15)
         ? text.substring(0, length ? length : 15) + "..."
         : text}
     </span>
