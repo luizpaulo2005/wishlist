5b80dc51b0897d99b2738832993993358e2dc14d
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2024-07-11 20:26:38 -0300

    feat: create file provider and auth provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "@/app/providers";
 
 // Fonte padrão da aplicação
 const inter = Inter({ subsets: ["latin"] });
@@ -8,13 +9,18 @@ const inter = Inter({ subsets: ["latin"] });
 // Metadados da aplicação
 export const metadata: Metadata = {
   title: "Lista de Desejos",
-  description: "Uma lista de desejos criada para fins de treino de aprendizado.",
+  description:
+    "Uma lista de desejos criada para fins de treino de aprendizado.",
 };
 
-export default function RootLayout({ children }: Readonly<{children: React.ReactNode}>) {
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <AuthProvider>
+        <body className={inter.className}>{children}</body>
+      </AuthProvider>
     </html>
   );
-};
+}
