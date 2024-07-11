5c087b6ebbe73906681fe8c3e127f5356bf393c7
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2024-07-11 20:37:20 -0300

    feat: add toaster component to root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider, ThemeProvider } from "@/app/providers";
+import { Toaster } from "@/components/ui/sonner";
 
 // Fonte padrão da aplicação
 const inter = Inter({ subsets: ["latin"] });
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="pt-br">
       <AuthProvider>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Toaster richColors />
           <body className={inter.className}>{children}</body>
         </ThemeProvider>
       </AuthProvider>
