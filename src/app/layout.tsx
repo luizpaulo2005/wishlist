ea812ce99d6e362040474d454cd5919b2ba12d33
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2024-07-11 20:35:12 -0300

    feat: add shadcn theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { AuthProvider } from "@/app/providers";
+import { AuthProvider, ThemeProvider } from "@/app/providers";
 
 // Fonte padrão da aplicação
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <AuthProvider>
-        <body className={inter.className}>{children}</body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <body className={inter.className}>{children}</body>
+        </ThemeProvider>
       </AuthProvider>
     </html>
   );
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,14 @@
 "use client"
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider as ColorProvider } from "next-themes";
+import type { ThemeProviderProps } from "next-themes/dist/types";
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   return <SessionProvider>{children}</SessionProvider>;
 };
 
-export { AuthProvider };
+const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
+  return <ColorProvider {...props}>{children}</ColorProvider>;
+};
+
+export { AuthProvider, ThemeProvider };
