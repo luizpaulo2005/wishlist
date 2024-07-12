e8f6b39cef94564eaad4ff25cc11a8ef9929193f
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2024-07-12 08:42:30 -0300

    feat: Update navbar component to use dark mode for shopping bag icon

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className="flex py-2 max-w-3xl pl-px mx-auto justify-between">
       <div className="flex items-center gap-2">
-        <ShoppingBag className="size-5 text-zinc-100" />
+        <ShoppingBag className="size-5 dark:text-zinc-100" />
         <span className="font-semibold">Wishlist</span>
       </div>
       <User />
